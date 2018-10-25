d37746f57972571faba9292d62c22d37f8b26e69
mwomack <mikewomack2000@yahoo.com>
2018-10-25 10:47:55 -0700

    basic finished

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,39 +2,89 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 var cTable = require("console.table")
 
-var con = mysql.createConnection({
-    host: "localhost",
-  
-    // Your port; if not 3306
-    port: 8889,
-  
-    // Your username
-    user: "root",
-  
-    // Your password
-    password: "root",
-    database: "bamazonDB"
-  });
-  
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("connected")
-    showTable();
-    // StaticRange();
-  });
 
-  // function to display inventory of items
-  function showTable() {
-    con.query("SELECT * FROM products", function(err, res){
-      if (err) throw err;
-      for (let i = 0; i < res.length; i++) {
-        console.log(`
-        item_id            product_name           department_name          price            stock_quantity
-        ----------      --------------------     --------------------     --------------   ----------------------------
-        ${res[i].item_id} ${res[i].product_name} ${res[i].department_name} ${res[i].price} ${res[i].stock_quantity}
-        `)
-        
-      }
-      // console.log(res[0].product_name);
-    })
-  }
+var con = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 8889,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "root",
+  database: "bamazonDB"
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("connected")
+  showTable();
+});
+
+// function to display inventory of items
+function showTable() {
+  con.query("SELECT * FROM products", function (err, res) {
+    if (err) throw err;
+    for (let i = 0; i < res.length; i++) {
+      console.table([
+        {
+          ITEM_ID: res[i].item_id,
+          PRODUCT_NAME: res[i].product_name,
+          DEPARTMENT_NAME: res[i].department_name,
+          PRICE: res[i].price,
+          STOCK_QUANTITIY: res[i].stock_quantity
+        }
+      ])
+    }
+    inquirer
+      .prompt([
+        {
+          name: "items",
+          type: "number",
+          message: "Enter Id of item you would like to buy"
+        },
+        {
+          name: "quantity",
+          type: "number",
+          message: "How many would you like to buy?",
+        }
+      ])
+      .then(function (answer) {
+        var chosenItem;
+        // Update stock quntity
+      
+        for (let i = 0; i < res.length; i++) {
+          if (res[i].item_id === answer.items) {
+            chosenItem = res[i];
+          }
+
+        }
+        console.log(chosenItem.stock_quantity)
+        if (chosenItem.stock_quantity > answer.quantity) {
+          var newQuant = chosenItem.stock_quantity - answer.quantity;
+          var total = chosenItem.price * answer.quantity;
+          con.query(
+            "UPDATE products SET ? WHERE ?", [{stock_quantity: newQuant}, {item_id: chosenItem.item_id}], function (error, result) {
+              if (error) throw err;
+              console.log("SUCCESSFUL PURCHASE");
+              console.log(`You've been billed: $${total}.`)
+              con.end()
+            }
+          );
+        }
+        else {
+          console.log("SORRY, INSUFFICIENT INVENTORY")
+          con.end()
+        }
+      })
+      
+  })
+}
+
+
+
+
+
+
--- schema.sql
+++ schema.sql
@@ -1,61 +1,18 @@
 DROP DATABASE IF EXISTS bamazonDB;
 CREATE database bamazonDB;
 
-USE bamazoznDB;
+USE bamazonDB;
 
 CREATE table products(
-    item_id INT NOT NULL,
+    item_id INT AUTO_INCREMENT NOT NULL,
     product_name VARCHAR(50),
     department_name VARCHAR(50),
     price DECIMAL(10,2),
-    stock_quantity INT(10) NULL
+    stock_quantity INT(10) NULL,
+     PRIMARY KEY(item_id)
 );
 
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Fujifilm X-T3(body)", "Cameras", 1100, 25);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Canon 5D MarkIV(body", "Cameras", 3500, 10);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("GoPro HERO 7", "Cameras", 399, 50);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Fujifilm GFX 50R(body)", "Cameras", 4499, 8);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Canon EF 70-200mm f/2.8L Telephoto)", "Lenses", 1799, 4);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Canon EF 24-70mm f/2.8L Zoom", "Lenses", 1699, 7);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Fujifilm XF 10-24mm f/4.0 R OIS", "Cameras", 999, 12);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Fujifilm GF 110mm f/2.0 R LM WR", "Lenses", 2139, 25);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Speedlite 430EX III-RT Flash", "Flashes", 249, 17);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("EF-X500 Shoe Mount Flash", "Flashes", 399, 9);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("64GB Extreme PRO", "Memory Cards", 34.99, 20);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Lacie 5TB Rugged USB", "Portable Storage Devices", 189, 6);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Mavic 2 Pro Drone", "Drones", 1499, 6);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Inspire 2 Drone with CinemaDNG", "Drones", 6198, 6);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Spark Drone", "Drones", 399, 11);
 
 
-SELECT * FROM bamazonDB.products;
+-- SELECT * FROM bamazonDB.products;
